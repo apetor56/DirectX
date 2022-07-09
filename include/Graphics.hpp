ceb4dba99dd655e1d586dbe0d3256c318ce3ef56
lukasz <lukten.github@gmail.com>
2022-07-09 19:52:50 +0200

    new classes for exceptions

--- include/Graphics.hpp
+++ include/Graphics.hpp
@@ -2,10 +2,47 @@
 #define GRAPHICS_HPP
 
 #include "RWin.hpp"
+#include "DXException.hpp"
 #include <d3d11.h>
+#include <vector>
 
 class Graphics {
 public:
+    class Exception : public DXException
+	{
+		using DXException::DXException;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException( int line,const char* file,HRESULT hr,std::vector<std::string> infoMsgs = {} ) noexcept;
+		const char* what() const noexcept override;
+		const char* getType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		HRESULT hr;
+		std::string info;
+	};
+	class InfoException : public Exception
+	{
+	public:
+		InfoException( int line,const char* file,std::vector<std::string> infoMsgs ) noexcept;
+		const char* what() const noexcept override;
+		const char* getType() const noexcept override;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		std::string info;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* getType() const noexcept override;
+	private:
+		std::string reason;
+	};
+
     Graphics(HWND hWnd);
 
     ~Graphics();
